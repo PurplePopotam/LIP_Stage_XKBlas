ff013d29fbe0bd159347f79c631deb1b33d3420a
Thibaut Modrzyk <52449813+PurplePopotam@users.noreply.github.com>
2021-05-11 12:14:55 +0200

    Sum of the dot product done on the host.
    Execution time measures, both on device and host.
    50 times longer on the CPU.

--- src/dotProduct.cu
+++ src/dotProduct.cu
@@ -3,22 +3,41 @@
 #include <iostream>
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
+#include <chrono>
 
+//Pour le moment la partie multiplication du produit scalaire est effectuée sur device, 
+//mais la somme est encore sur l'hote...
 
 __global__ void produitPasScalaire(float* a, float* b, float* c, int n) {
-
-    float res;
 
     int tid = (blockIdx.x * blockDim.x) + threadIdx.x;
 
     if (tid < n) {
         c[tid] = a[tid] * b[tid];
     }
-
 }
 
+float sum(float* a, int n) {
+    float res = 0.0f;
+
+    for (int i = 0; i < n; i++) {
+        res += a[i];
+    }
+
+    return res;
+}
+
+
 int main() {
-    int N = 1 << 16;
+
+    //Evènement CUDA pour mesurer le temps d'execution sur le GPU
+    cudaEvent_t startGPU;
+    cudaEvent_t stopGPU;
+    cudaEventCreate(&startGPU);
+    cudaEventCreate(&stopGPU);
+    float milliseconds;
+
+    int N = 20000000;
 
     float* h_a, * h_b, * h_c;
     float* d_a, * d_b, * d_c;
@@ -34,8 +53,8 @@ int main() {
     cudaMalloc(&d_c, bytes);
 
     for (size_t i = 0; i < N; i++) {
-        h_a[i] = rand();
-        h_b[i] = rand();
+        h_a[i] = rand() % 100;
+        h_b[i] = rand() % 100;
         h_c[i] = 0;
     }
 
@@ -45,17 +64,31 @@ int main() {
     int NUM_THREADS = 256;
     int NUM_BLOCKS = (int)ceil(N / NUM_THREADS);
 
+    //On fait les multiplication sur le GPU
+    cudaEventRecord(startGPU);
     produitPasScalaire <<<NUM_BLOCKS, NUM_THREADS>>> (d_a, d_b, d_c, N);
+    cudaEventRecord(stopGPU);
 
     cudaMemcpy(h_c, d_c, bytes, cudaMemcpyDeviceToHost);
 
-    std::cout << "finished succesfully" << std::endl;
+    cudaEventSynchronize(stopGPU);
 
-    for (size_t i = 0; i < 10; i++) {
-        std::cout << h_a[i] << " * " << h_b[i] << " = " << h_c[i] << std::endl;
-    }
+    //On fait la somme sur l'hote
+    auto startCPU = std::chrono::high_resolution_clock::now();
+    float res = sum(h_c, N);
+    auto stopCPU = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double, std::milli> millisecondsCPU = stopCPU - startCPU;
+
+    cudaEventElapsedTime(&milliseconds, startGPU, stopGPU);
+    
+
+    std::cout << "Resultat du produit scalaire : " << res << std::endl;
+
+    std::cout << "Temps d'execution GPU : " << milliseconds << " ms . " << std::endl;
+
+    std::cout << "Temps d'execution CPU : " << millisecondsCPU.count() << " ms ." << std::endl;
 
     std::cout << std::endl;
-
     return 0;
 }
